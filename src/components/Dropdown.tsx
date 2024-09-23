b159321d79feb4dd3ad14378500b1e0f53c16790
sddltx <scott.drummond@dltx.io>
2024-09-23 18:11:49 +1000

    feat: handle keyboard focus for dropdown

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import classnames from "classnames";
 
+const TAB_KEY = 9;
+
 type DropdownProps = {
     items: string[];
     initial?: string;
@@ -17,13 +19,18 @@ const Dropdown: React.FC<DropdownProps> = ({ items, initial, onClick }) => {
         onClick(item);
     }
 
+    const lostKeyFocus = <T extends {}>({ which, shiftKey }: React.KeyboardEvent<T>, index: number) => {
+        if ((which == TAB_KEY) && ((index == 0 && shiftKey) || (index == (items.length - 2) && !shiftKey)))
+            setOpen(false);
+    }
+
     // Can't use <select> here as notoriously difficult to style
     return (<div className="relative">
-        <button className={classnames("bg-transparent", { "underline underline-offset-4 decoration-[#E6FF02]": open })} onClick={() => setOpen(o => !o)}>{selected}</button>
-        {open && <ul className="absolute top-[1.1rem] -left-[5px] bg-black z-10" onMouseLeave={() => setOpen(false)}>
+        <button className={classnames("bg-transparent", { "underline underline-offset-4 decoration-dltx-green": open })} onClick={() => setOpen(o => !o)}>{selected}</button>
+        {open && (items.length > 1) && <ul className="absolute top-[1.1rem] -left-[5px] bg-black z-10" onMouseLeave={() => setOpen(false)}>
             {items.filter(item => item != selected).map((item, i) => (
-                <li key={i} className="text-[#818181] text-nowrap mx-[5px] my-[2px] hover:text-white">
-                    <button className="bg-transparent font-normal text-start w-full" onClick={() => clicked(item)}>{item}</button>
+                <li key={i} className="text-dltx-grey text-nowrap mx-[5px] my-[2px] hover:text-white">
+                    <button className="bg-transparent font-normal text-start w-full" onClick={() => clicked(item)} onKeyDown={(e) => lostKeyFocus(e, i)}>{item}</button>
                 </li>
             ))}
         </ul>}
