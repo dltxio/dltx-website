9162d83c731bada40d34afd7c33d9b0463ccb0ac
Scott Drummond <scott.drummond@dltx.io>
2024-10-18 11:35:39 +1000

    chore: fix name

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import PageLayout from "../components/PageLayout";
 import WarningImg from "../assets/warning.svg";
 
-const Home: React.FC = () => {
+const NotFound: React.FC = () => {
     return (
         <PageLayout>
             <div className="flex justify-center items-center h-full text-lg"><img className="mr-2" width={32} src={WarningImg} />Page not found</div>
@@ -10,4 +10,4 @@ const Home: React.FC = () => {
     );
 };
 
-export default Home;
+export default NotFound;
